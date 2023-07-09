7c53cfcb11579639ecb37e9b4fc687c02e87b8a5
M0NDI <armond_93@live.co.uk>
2023-07-09 22:48:38 +0100

    changed color styling of elements to better match darker background

--- src/api/TMDB-Api.js
+++ src/api/TMDB-Api.js
@@ -139,15 +139,15 @@ export const FetchTopRated = async (page) => {
   return response.data;
 };
 
-export const fetchMovieGenres = async () => {
-  const response = await axios.get(`${BASE_URL}/genre/movie/list`, {
-    headers: {
-      Authorization: BEARER,
-    },
-    params: {
-      api_key: API_KEY,
-    }
-  })
-  console.log(response.data)
-  return response.data;
-}
+// export const fetchMovieGenres = async () => {
+//   const response = await axios.get(`${BASE_URL}/genre/movie/list`, {
+//     headers: {
+//       Authorization: BEARER,
+//     },
+//     params: {
+//       api_key: API_KEY,
+//     }
+//   })
+//   console.log(response.data)
+//   return response.data;
+// }

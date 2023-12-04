5e3d2a8f56824c7b4f2d7a83cd6252eeeefcaf72
m0ndi <armond_93@live.co.uk>
2023-12-04 18:37:48 +0000

    removed: removed scrollToTop prop that was unnecessarily being passed to MoviePage

--- src/App.js
+++ src/App.js
@@ -94,7 +94,6 @@ function App() {
                 element={
                   <MoviePage
                     isSearchSubmitted={isSearchSubmitted}
-                    scrollToTop={scrollToTop}
                   />
                 }
               />
